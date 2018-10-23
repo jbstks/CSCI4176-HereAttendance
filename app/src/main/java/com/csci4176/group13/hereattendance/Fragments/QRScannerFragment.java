c5d083357bd2503601a7fee58eb8ee54e2bd6cd4
Abdullah Negahban <ab923837@dal.ca>
2018-10-23 11:51:38 -0300

    Implemented QR scanner view
    Implemented the QR scanner view to allow the users to scan the QR Code. The functionality has not been added in yet.

--- app/src/main/java/com/csci4176/group13/hereattendance/Fragments/QRScannerFragment.java
+++ app/src/main/java/com/csci4176/group13/hereattendance/Fragments/QRScannerFragment.java
@@ -1,11 +1,20 @@
 package com.csci4176.group13.hereattendance.Fragments;
 
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.csci4176.group13.hereattendance.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.w3c.dom.Text;
 
 /**
  * Fragment for the QR camera scanner
@@ -16,6 +25,13 @@ public class QRScannerFragment extends android.support.v4.app.Fragment {
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
+
+    CameraSource camera;
+    SurfaceView qrCodeView;
+    TextView qrResult;
+    BarcodeDetector qrDetect;
+    int cameraPermission = 007;
+
     public QRScannerFragment() {
     }
 
@@ -23,7 +39,32 @@ public class QRScannerFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.fragment_qrscanner, container, false);
+        qrCodeView = view.findViewById(R.id.QRView);
+        qrResult = view.findViewById(R.id.txt);
+        qrDetect = new BarcodeDetector.Builder(view.getContext())
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+        camera = new CameraSource.Builder(view.getContext(), qrDetect)
+                .setRequestedPreviewSize(640, 640).build();
+
+        qrCodeView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
         return view;
     }
 }
