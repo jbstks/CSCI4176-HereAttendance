ac73d8693dcad05ab5c27d854f300030cbb9a599
dhruvishah1995 <dnshah1995@gmail.com>
2018-10-31 15:29:47 -0300

    Update app/src/main/java/com/csci4176/group13/hereattendance/Fragments/Professor/QRGeneratorFragment.java
    Comments added

--- app/src/main/java/com/csci4176/group13/hereattendance/Fragments/Professor/QRGeneratorFragment.java
+++ app/src/main/java/com/csci4176/group13/hereattendance/Fragments/Professor/QRGeneratorFragment.java
@@ -35,7 +35,7 @@ import org.w3c.dom.Text;
 import java.io.IOException;
 
 /**
- * Fragment for the QR camera scanner
+ * Fragment for the QR generator
  */
 public class QRGeneratorFragment extends android.support.v4.app.Fragment {
 
@@ -43,25 +43,26 @@ public class QRGeneratorFragment extends android.support.v4.app.Fragment {
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
+    public QRGeneratorFragment() { }
 
     Button btnQRGenerate ;
     EditText edtQRCSCIText,edtQRDateText;
     ImageView imgQRCode;
 
-    public QRGeneratorFragment() {
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
+        /**
+         *Getting the layout for QR Generator
+         */
         View view = inflater.inflate(R.layout.fragment_qrgenerator, container, false);
+
         btnQRGenerate=view.findViewById(R.id.btnQRGenerate);
         edtQRCSCIText=view.findViewById(R.id.edtQRCSCIText);
         edtQRDateText=view.findViewById(R.id.edtQRDateText);
         imgQRCode=view.findViewById(R.id.imgQRCode);
+
+        // OnClickListener for generating QR code
             btnQRGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,12 +71,16 @@ public class QRGeneratorFragment extends android.support.v4.app.Fragment {
                     String strCSCI = edtQRCSCIText.getText().toString();
                     String strDateLect = edtQRDateText.getText().toString();
                     String QRText = strCSCI.concat("_"+strDateLect);
-                    Log.d("QRGEN", "Hey I made it: this is QRText="+QRText);
+                    Toast.makeText(getContext(),QRText,Toast.LENGTH_SHORT).show();
+
+                    //Generating QR Code For QRtext
                     BitMatrix bitMatrix= multiFormatWriter.encode(QRText, BarcodeFormat.QR_CODE,200,200);
                     BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
                     Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                    //setting QR Code to Image view
                     imgQRCode.setImageBitmap(bitmap);
-                } catch (Exception e) {
+                    } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
