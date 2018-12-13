6c1d4fb69530ab4dc767e061bfbcf2302d8ef554
Abdullah Negahban <ab923837@dal.ca>
2018-12-13 16:05:02 -0400

    Added animation to map view
    when user opens the map, the application automatically zooms to their location

--- app/src/main/java/com/csci4176/group13/hereattendance/Fragments/Student/Maps.java
+++ app/src/main/java/com/csci4176/group13/hereattendance/Fragments/Student/Maps.java
@@ -2,10 +2,13 @@ package com.csci4176.group13.hereattendance.Fragments.Student;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -19,11 +22,13 @@ import android.widget.Toast;
 import com.csci4176.group13.hereattendance.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -37,7 +42,8 @@ public class Maps extends Fragment implements OnMapReadyCallback {
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private static final float DEFAULT_ZOOM = 15f;
+    private double longitude;
+    private double latitude;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -71,6 +77,9 @@ public class Maps extends Fragment implements OnMapReadyCallback {
                     Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
+            LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Location userLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            
             LatLng CS = new LatLng(44.637444, -63.587224);
             LatLng LSC = new LatLng(44.636228, -63.594058);
             LatLng Tupper = new LatLng(44.639354, -63.583841);
@@ -80,6 +89,12 @@ public class Maps extends Fragment implements OnMapReadyCallback {
             mMap.addMarker(new MarkerOptions().position(Tupper).title("CSCI3110").snippet("Algorithms"));
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            LatLng ltlg = new LatLng(userLocation.getLatitude(),userLocation.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(ltlg, 17);
+            mMap.animateCamera(cameraUpdate);
+
+
+
 
         }
     }
